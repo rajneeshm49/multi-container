7a461a225fd28901d08d426c841c713104ba9458
rajneesh-nielsen <rajneesh.mishra@nielsen.com>
2018-11-17 13:12:01 +0530

    commiting client files

---
+++ client-ng6/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeService } from '../services/home.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+
+  blog = {hero: ''};
+  error_msg = '';
+    
+  constructor(private homeService: HomeService) { }
+
+  onSubmit(): void {
+    this.homeService.displayHomePage(this.blog).subscribe(
+      res => {
+        if(res.success === false) {
+          this.error_msg = 'Some error occurred while creating blog. Please try again later';
+        } else {
+          //this.router.navigate(['/']);
+          console.log(res);
+          this.title = res.mykey;
+        }
+      }
+    );
+  }
+}
