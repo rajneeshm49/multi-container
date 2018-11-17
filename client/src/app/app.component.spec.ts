08f2e884d95e04e83afaa4f8d41c9639466cd661
rajneesh-nielsen <rajneesh.mishra@nielsen.com>
2018-11-17 15:43:57 +0530

    tests removed

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -7,7 +7,7 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
-  }));
+  })); 
   // it('should create the app', async(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   const app = fixture.debugElement.componentInstance;
